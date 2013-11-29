f11722452152e69931d45efff3a305e8e2097855
angbrav <manuelbravogestoso@gmail.com>
2013-11-29 18:13:44 +0100

    initial commit

---
+++ apps/mfmn/src/mfmn.erl
@@ -0,0 +1,30 @@
+-module(mfmn).
+-include("mfmn.hrl").
+-include_lib("riak_core/include/riak_core_vnode.hrl").
+
+-export([
+         ping/0,
+	 put/2,
+	 get/1
+        ]).
+
+%% Public API
+
+%% @doc Pings a random vnode to make sure communication is functional
+ping() ->
+    DocIdx = riak_core_util:chash_key({<<"ping">>, term_to_binary(now())}),
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, mfmn),
+    [{IndexNode, _Type}] = PrefList,
+    riak_core_vnode_master:sync_spawn_command(IndexNode, ping, mfmn_vnode_master).
+
+put(Key, Value)->
+    DocIdx = riak_core_util:chash_key({<<"key">>, term_to_binary(Key)}),
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, mfmn),
+    [{IndexNode, _Type}] = PrefList,
+    riak_core_vnode_master:sync_spawn_command(IndexNode, {put, Key, Value}, mfmn_vnode_master).
+
+get(Key)->
+    DocIdx = riak_core_util:chash_key({<<"key">>, term_to_binary(Key)}),
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, mfmn),
+    [{IndexNode, _Type}] = PrefList,
+    riak_core_vnode_master:sync_spawn_command(IndexNode, {get, Key}, mfmn_vnode_master).
